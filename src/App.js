42e35ee994d8f2087a6cbc2164048363d52bfcd6
SoheilRezaei <soheilrezaei966+github@gmail.com>
2023-07-16 22:30:10 -0400

    awaiting a fetch request

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 export default function MyApp() {
+
+  const [advice, setAdvice] = useState("");
+
+  async function getAdvice() {
+    const res = await fetch("https://api.adviceslip.com/advice");
+    const data = await res.json();
+    setAdvice(data.slip.advice);
+  }
+
   return (
     <div>
-      <h1>Hello World!</h1>
-      <button>Click me!</button>
+      <h1>{advice}</h1>
+      <button onClick={getAdvice}>Get Advice!</button>
     </div>
   );
 }
