2ac1a75249cb98500aa1d7f24c2dd01ec778993c
SoheilRezaei <soheilrezaei966+github@gmail.com>
2023-07-16 22:22:31 -0400

    hello world

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
-import { useEffect, useState } from "react";
-
-function MyButton() {
-  return (
-    <button>My Button</button>
-  );
-}
-
-
 export default function MyApp() {
-
-  const [advice, setAdvice] = useState("");
-  const [count, setCount] = useState(0);
-
-  async function getAdvice() {
-    const res = await fetch("https://api.adviceslip.com/advice");
-    const data = await res.json();
-    setAdvice(data.slip.advice);
-    setCount((c) => c+1);
-  }
-
-  useEffect(function() {
-    getAdvice()
-  }, []);
-
   return (
     <div>
-      <h1>{advice}</h1>
-      <button onClick={getAdvice}>Get advice</button>
-      <p>You have already read <strong>{count}</strong> advices!</p>
-      <MyButton />
+      <h1>Hello World!</h1>
+      <button>Click me!</button>
     </div>
   );
 }
